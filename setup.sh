39564db6be2cddab1d31a1d693470600b2a1fa9c
daraccrafter <darkovujica01@gmail.com>
2024-06-23 01:58:01 +0200

    final submodule

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y python3-pandas python3-matplotlib psutil
+sudo apt install -y python3-pandas python3-matplotlib python3-psutil
 git submodule update --init --recursive
 make -C redis & make -C redis-io_uring
 cp redis/src/redis-benchmark .
