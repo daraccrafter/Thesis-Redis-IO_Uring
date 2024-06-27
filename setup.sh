f8e05f37321f2243d5891acce194e5ab115b5b17
daraccrafter <darkovujica01@gmail.com>
2024-06-27 18:05:22 +0200

    rm old files

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt install -y python3 python3-pandas python3-matplotlib python3-psutil
-git submodule update --init --recursive
-make -C redis & make -C redis-io_uring
-cp redis/src/redis-benchmark .
-cp redis/src/redis-check-aof .
