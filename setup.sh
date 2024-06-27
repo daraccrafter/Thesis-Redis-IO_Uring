f68a45c862da20a5f87448ead88cea7fc42e8650
daraccrafter <darkovujica01@gmail.com>
2024-06-27 18:33:27 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 sudo apt install -y build-essential  python3 python3-pandas python3-matplotlib python3-psutil python3-redis -y
 git submodule update --init --recursive
 make -C redis & make -C redis-io_uring
-cp redis/src/redis-benchmark src/
-cp redis/src/redis-check-aof src/
+cp redis/src/redis-benchmark scripts/
+cp redis/src/redis-check-aof scripts/
